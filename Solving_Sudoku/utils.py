b68c77c27b1627b812c59330179d35751f4a159b
Yu Zou <github@zouyu.com>
2017-02-19 19:57:58 +0800

    naked twin strategy

--- Solving_Sudoku/utils.py
+++ Solving_Sudoku/utils.py
@@ -13,19 +13,22 @@ column_units = [cross(rows, y) for y in cols]
 square_units = [cross(xs, ys) for xs in ['ABC', 'DEF', 'GHI'] for ys in ['123', '456', '789']]
 unitlist = row_units + column_units + square_units
 units = dict((box, [u for u in unitlist if box in u]) for box in boxes)
-peers = dict((box, set(sum(units[box], [])) - set([box])) for box in boxes)
+peers = dict((box, set(sum(units[box], [])) - {box}) for box in boxes)
 
 
 def display(values):
     """Display the values as a 2-D grid.
     Input: The sudoku in dictionary form
     Output: None"""
+    if values is None:
+        print('illegal puzzle grid')
+        return
     width = 1 + max(len(values[box]) for box in boxes)  # should be 2
     line = '+'.join(['-' * (width * 3)] * 3)
     for r in rows:
         print(''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in cols))
         if r in 'CF': print(line)
-    return
+    print()
 
 
 if __name__ == '__main__':
